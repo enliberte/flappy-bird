b9349fc44749cdaf0fef80bfb2139998b47aa4a8
enliberte <zugife21@gmail.com>
2018-07-09 17:57:45 +0300

    game

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ app = express();
 app.use('/audio', express.static(__dirname + '/audio'));
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/js', express.static(__dirname + '/js'));
+app.use('/', express.static(__dirname));
 
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
 console.log('Server started');
