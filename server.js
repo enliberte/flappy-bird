566bfdf5741f2565778485d694ab182f37d721e1
enliberte <zugife21@gmail.com>
2018-07-09 19:51:54 +0300

    game

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 express = require('express');
 app = express();
+port = process.env.PORT || 8000;
 
 //отдача статики
 app.use('/audio', express.static(__dirname + '/audio'));
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/js', express.static(__dirname + '/js'));
-app.use(favicon(__dirname + '/public/yourfavicon.ico'));
+/*app.use(favicon(__dirname + '/public/yourfavicon.ico'));*/
 
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
-app.listen(process.env.PORT);
-console.info('Server started');
+app.post('/', function (request, response) {
+    response.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, function () {
+    console.log('App is running on port' + port);
+});
