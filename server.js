1c4b0a2f77653140eec1959ca350e7b5691d5efd
enliberte <zugife21@gmail.com>
2018-08-01 21:19:14 +0300

    game

--- server.js
+++ server.js
@@ -4,21 +4,6 @@ const { Client } = require('pg');
 app = express();
 port = process.env.PORT || 8000;
 
-//создаем подключение к БД
-client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true}
-);
-client.connect();
-
-client.query('SELECT name FROM users;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-    }
-   client.end();
-});
-
 //отдача статики
 app.use('/audio', express.static(__dirname + '/audio'));
 app.use('/img', express.static(__dirname + '/img'));
@@ -31,4 +16,18 @@ app.get('/', function (request, response) {
 
 app.listen(port, function () {
     console.log('App is running on port ' + port);
+    //создаем подключение к БД
+    client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true}
+    );
+    client.connect();
+
+    client.query('SELECT name FROM users;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
 });
